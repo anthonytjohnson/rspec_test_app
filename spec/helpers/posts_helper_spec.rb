efa7d6174bf5720212674445a121beb7c0a229aa
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-09-16 14:19:23 +0900

    Test helper

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,15 +1,13 @@
-require 'rails_helper'
-
-# Specs in this file have access to a helper object that includes
-# the PostsHelper. For example:
-#
-# describe PostsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
+include PostsHelper
 RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'assigns a user to a post' do
+    creator = User.first_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
+    @post = Post.new(
+      title: 'MyString',
+      body: 'MyText',
+      views: 1
+    )
+    assign_post_creator(@post, creator)
+    expect(@post.user).to be(creator)
+  end
 end
